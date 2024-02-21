c515860f1506c1ac3715a8520097299230df7bec
Guian Rocha <guian.rocha@t-systems.com>
2024-02-21 08:01:56 -0300

    criado entidades

---
+++ Domain/Entities/Equipamento.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Equipamento
+    {
+        public int Id { get; set; }
+        public EnumTipoEquipamento TipoEquipamento { get; set; }
+        public string? SerialNumber { get; set; }
+        public string? Hostname { get; set; }
+        public bool Cadeado { get; set; }
+    }
+}
